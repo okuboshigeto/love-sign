e029eab643ca5a5f47185ed8a443efce4eae9818
Shigeto Okubo <168151882+okuboshigeto@users.noreply.github.com>
2025-04-16 11:19:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,5 @@ const Obniz = require("obniz");
 const obniz = new Obniz(obnizPath);
 
 obniz.onconnect = async function () {
-    const led = obniz.wired("LED", {
-        // 記述
-    });
-    
+    // 記述    
 };
